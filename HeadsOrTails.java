106cde72ad28095035cd3d3eed60a5f9d0a6cb66
Arinosu <136328111+Arinosu-MY@users.noreply.github.com>
2024-06-28 16:50:55 +0900

    Hot: initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class HeadsOrTails{
+    public static void main(String[] args) {
+
+        int heads_count = 0;
+        int tails_count = 0;
+        Random random = new Random();
+
+        System.out.println("Tossing a coin...");
+
+        for(int i = 1; i <= 3; i++){
+            System.out.print("Round " + i + ": ");
+            if(random.nextBoolean() == true){
+                System.out.println("Heads");
+                heads_count++;
+            }else{
+                System.out.println("Tails");
+                tails_count++;
+            }
+        }
+
+        System.out.println("Heads: " + heads_count + ", Tails: " + tails_count);
+    }
+}
