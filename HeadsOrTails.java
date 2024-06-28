806da9eecebd1ebea59dc5898f6da7a9e2916a17
Arinosu <136328111+Arinosu-MY@users.noreply.github.com>
2024-06-28 17:17:44 +0900

    User name added

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -1,11 +1,19 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class HeadsOrTails{
     public static void main(String[] args) {
 
         int heads_count = 0;
         int tails_count = 0;
+        String name;
         Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Who are you?");
+        name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
 
         System.out.println("Tossing a coin...");
 
@@ -21,5 +29,6 @@ public class HeadsOrTails{
         }
 
         System.out.println("Heads: " + heads_count + ", Tails: " + tails_count);
+        sc.close();
     }
 }
